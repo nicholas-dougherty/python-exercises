3a913b8d934c8706ecf60e1a3cbf1cae6f90589c
Nicholas Dougherty <nicholas.j.dougherty@gmail.com>
2022-01-18 16:57:00 -0600

    Worked through this variables exercises and made some active attempts to make it clean, easy to read and well organized. Couldn't figure out the bonus element of the final problem but otherwise it went well.

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -33,4 +33,82 @@ print('This week I am banking', paycheck, 'dollars!')
 A student can be enrolled to a class only if the class is not full and the class 
 schedule does not conflict with her current schedule.
 '''
+def elibility():
+    pass
+eligible = 'Class added to schedule.'
+ineligible = 'Error. See enrollment manual.'
+# set Boolean values for these statements to false. 
+seats_unavailable = 0
+classes_conflict = 0
+# if either case is present, FALSE, or failure to enroll. 
+if seats_unavailable or classes_conflict:
+    eligibility()
+    print(ineligible)
+else:
+    print(eligible)
 
+
+# 4.)
+'''
+A product offer can be applied only if people buys more than 2 items, and the offer
+ has not expired. Premium members do not need to buy a specific amount of products.
+'''
+def offer():
+    pass
+success = 'Thanks for shopping with us.'
+failure = 'Go on. GET. not git. GET!'
+# the only real stipulations here. 
+cart_minimum = 3
+premier_deal = 1
+# Me as a shopper.
+cart_count = 69
+premier_membership = 0
+
+if(cart_count >= cart_minimum 
+    or premier_deal != 0):
+    offer()
+    print(success)
+else:
+    print(failure)
+
+# experiencing a syntax error when attempting to move or to the subsequent line
+# made the mistake of using a colon instead of equal sign when creating variables
+#                                                                            No clue why.
+#
+
+# 5.) 
+''' Use the following code to
+follow the instructions below.
+    username = 'codeup'
+    password = 'notastrongpassword'
+Create a variable that holds a boolean values for each of the following conditions: 
+a.) the password must be at least 5 characters
+b.) the username must be no more than 20 characters
+c.) the password must not be the same as the username
+(B-B-B-BONUS) neither the username or password can start or end with whitespace.
+'''
+
+def registration():
+    pass
+
+username = 'codeup'
+password = 'notastrongpassword' #meta
+
+password_minimum = 5
+username_maximum = 20
+
+grant_username = len(username) <= username_maximum
+grant_password = len(password) >= password_minimum
+grant_complete = username != password 
+# grant_stripper_king = 
+
+registered = 'Account creation complete.'
+rejected = 'Did not meet account criteria. Try again.'
+
+if(grant_username and grant_password and grant_complete):
+    registration()
+    print(registered)
+else:
+    print(rejected)
+
+# I will return and test for a whitespace strip later. 
