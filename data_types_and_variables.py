8d60ae43d5c0bf365565e5fad69df3e8cbef2a7a
Nicholas Dougherty <nicholas.j.dougherty@gmail.com>
2022-05-10 17:05:52 -0500

    backwards glances

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -1,3 +1,53 @@
+# .#####...####..######..####.........######.##..##.#####..######..####..       
+# .##..##.##..##...##...##..##..........##....####..##..##.##.....##.....       
+# .##..##.######...##...######..........##.....##...#####..####....####..       
+# .##..##.##..##...##...##..##..........##.....##...##.....##.........##.       
+# .#####..##..##...##...##..##..........##.....##...##.....######..####..       
+# ..............................................................................
+# ..............................................................................
+# .######.######.######.######.######.######.######.######.######.######.######.
+# ..............................................................................
+# ..............................................................................
+# ..............................................................................
+# ..####..#####..######.#####...####..######..####..#####...####..              
+# .##..##.##..##.##.....##..##.##..##...##...##..##.##..##.##.....              
+# .##..##.#####..####...#####..######...##...##..##.#####...####..              
+# .##..##.##.....##.....##..##.##..##...##...##..##.##..##.....##.              
+# ..####..##.....######.##..##.##..##...##....####..##..##..####..              
+# .......................................................................       
+# .......................................................................       
+# .######.######.######.######.######.######.######.######.######.######.       
+# .......................................................................       
+# .......................................................................       
+# .......................................................................       
+# .##..##..####..#####..######..####..#####..##.....######..####..              
+# .##..##.##..##.##..##...##...##..##.##..##.##.....##.....##.....              
+# .##..##.######.#####....##...######.#####..##.....####....####..              
+# ..####..##..##.##..##...##...##..##.##..##.##.....##.........##.              
+# ...##...##..##.##..##.######.##..##.#####..######.######..####..              
+# ................................................................              
+# 
+
+
+# .......................................................................       
+# .......................................................................  
+
+'''
+Create a Python script file named data_types_and_variables.py. 
+Inside it, write some Python code, that is, variables and operators, to describe the following scenarios. (Do not worry about the real operations to get the values, the goal of these exercises is to understand how real world conditions can be represented with code.)
+
+You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is 3 dollars, how much will you have to pay?
+
+Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays 400 dollars per hour, Amazon 380, and Facebook 350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+
+A student can be enrolled to a class only if the class is not full and the class schedule does not conflict with her current schedule.
+
+A product offer can be applied only if people buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
+'''
+# .......................................................................       
+# .......................................................................  
+
+
 # 1.)
 '''
 I've rented some movies for my kids: 
@@ -14,6 +64,9 @@ total_cost = daily_rental_fee * total_days_per_title
 print(total_cost, 'dollars is nothing. Anything for my imaginary kids.')
 # $27 
 
+# .......................................................................       
+# .......................................................................  
+
 # 2.)
 '''
 Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook,
@@ -27,6 +80,9 @@ google_payout = 400 * 6
 paycheck = google_payout + facebook_payout + amazon_payout 
 print('This week I am banking', paycheck, 'dollars!')
 # psh, I wish. 
+
+# .......................................................................       
+# .......................................................................  
 
 # 3.) 
 '''
@@ -47,6 +103,8 @@ if seats_unavailable or classes_conflict:
 else:
     print(eligible)
 
+# .......................................................................       
+# .......................................................................  
 
 # 4.)
 '''
@@ -75,6 +133,9 @@ else:
 # made the mistake of using a colon instead of equal sign when creating variables
 #                                                                            No clue why.
 #
+
+# .......................................................................       
+# .......................................................................  
 
 # 5.) 
 ''' Use the following code to
@@ -111,4 +172,5 @@ if(grant_username and grant_password and grant_complete):
 else:
     print(rejected)
 
-# I will return and test for a whitespace strip later. 
+# .......................................................................       
+# .......................................................................  
